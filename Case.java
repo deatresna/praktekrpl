b766daa26c976dbb78036599d331349e6d0ce88d
deatresna <dhea_usa@yahoo.com>
2014-09-17 20:26:09 +0700

    de

---
+++ Case.java
@@ -0,0 +1,33 @@
+import java.io.*;
+public class Nilai
+{
+	public static void main (String [] args)
+	{
+		String HM;
+		int NA=Integer.parseInt(args[1]);
+		
+		switch(NA/10)
+		
+		{
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+				System.out.println("E");
+		case 4:
+		case 5:
+				System.out.println("D");
+		case 6 :
+				System.out.println("C");
+		case 7 :
+				System.out.println("BC");
+		case 8 :
+		case 9 :
+		case 9 :
+		default : 
+		}
+			System.out.println("Nama Anda="+args[0]);
+		System.out.println("Nilai Huruf="+HM);
+	}}
+		
+		
